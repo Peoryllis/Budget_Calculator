59147a991ba31a2b4bc078aa03f9dcb7453cd1ce
Anaya Chinyere <136634937+Peoryllis@users.noreply.github.com>
2023-07-18 18:44:50 -0700

    Create calculator_main.py

---
+++ calculator_main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 18 18:34:30 2023
+
+Makes the main calculator where all the parts come together and makes one calculator, ig
+"""
+
+import tkinter as tk
+
+root = tk.Tk()
+root.geometry('1500x900')
+
+root.mainloop() #show the window
+
